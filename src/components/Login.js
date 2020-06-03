4b66bc64faf168e301c7adf89b14ebe93aec54c9
Jack Brown <brown_jack80@hotmail.com>
2020-06-03 11:51:25 +0100

    login complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,20 +6,34 @@ const Login = () => {
     <div className="wrapper">
       <form>
         <h1 className="title">LOGIN</h1>
-        <input type="text" placeholder="Email" className="input" />
-        <input type="text" placeholder="Password" className="input" />
+        <input
+          type="email"
+          placeholder="Email"
+          className="input email"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input password"
+          required
+        />
         <div className="forgot-wrapper">
           <a href="/">Forgot Password?</a>
         </div>
         <button type="submit" className="lets-go">
           LET'S GO
         </button>
+      </form>
+      <form>
         <div className="or-divider">
           <hr className="divider"></hr>
           <span>Or</span>
           <hr className="divider"></hr>
         </div>
-        <button className="input alt-login facebook">Continue with Facebook</button>
+        <button className="input alt-login facebook">
+          Continue with Facebook
+        </button>
         <br />
         <button className="input alt-login google">Continue with Google</button>
       </form>
